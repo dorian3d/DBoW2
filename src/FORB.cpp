e13fa950d358b3ca8ce91a3689fb0d0ef57a516b
Dorian Galvez-Lopez <dorian3d@gmail.com>
2020-05-06 15:39:44 +0200

    FORB: dead code removed

--- src/FORB.cpp
+++ src/FORB.cpp
@@ -187,38 +187,7 @@ void FORB::toMat32F(const std::vector<TDescriptor> &descriptors,
 
 void FORB::toMat32F(const cv::Mat &descriptors, cv::Mat &mat)
 {
-
   descriptors.convertTo(mat, CV_32F);
-  return; 
-
-  if(descriptors.empty())
-  {
-    mat.release();
-    return;
-  }
-  
-  const int N = descriptors.rows;
-  const int C = descriptors.cols;
-  
-  mat.create(N, FORB::L*8, CV_32F);
-  float *p = mat.ptr<float>(); // p[i] == 1 or 0
-  
-  const unsigned char *desc = descriptors.ptr<unsigned char>();
-  
-  for(int i = 0; i < N; ++i, desc += C)
-  {
-    for(int j = 0; j < C; ++j, p += 8)
-    {
-      p[0] = (desc[j] & (1 << 7) ? 1 : 0);
-      p[1] = (desc[j] & (1 << 6) ? 1 : 0);
-      p[2] = (desc[j] & (1 << 5) ? 1 : 0);
-      p[3] = (desc[j] & (1 << 4) ? 1 : 0);
-      p[4] = (desc[j] & (1 << 3) ? 1 : 0);
-      p[5] = (desc[j] & (1 << 2) ? 1 : 0);
-      p[6] = (desc[j] & (1 << 1) ? 1 : 0);
-      p[7] = desc[j] & (1);
-    }
-  } 
 }
 
 // --------------------------------------------------------------------------
